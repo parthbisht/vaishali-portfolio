4ae3cf118e7f07c036fcf1663a5643b856d09c34
parth raj bisht <parthbisht22@gmail.com>
2021-08-01 11:59:10 +0530

    last

--- src/component/nav.js
+++ src/component/nav.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 export default function nav() {
   return (
     <div>
- <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+ <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
